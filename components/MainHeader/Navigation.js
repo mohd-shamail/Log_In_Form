c803ebcad0690eb170e410788c1f3b83d7483441
mohd shamail <coolshamail4@gmail.com>
2023-08-05 23:28:13 +0530

    last commit

---
+++ components/MainHeader/Navigation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Home</a>
+          </li>
+        )}
+               {props.isLoggedIn && (
+          <li>
+            <a href="/">Profile</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
